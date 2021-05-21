dc4323a5b3c0e4392a190461f19104e51548274b
40206673 <cbrush01@qub.ac.uk>
2021-05-21 13:58:13 +0100

    Testing request

---
+++ stravaRequest.py
@@ -0,0 +1,26 @@
+import requests
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+auth_url = "https://www.strava.com/oauth/token"
+activites_url = "https://www.strava.com/api/v3/athlete/activities"
+
+payload = {
+    'client_id': "66386",
+    'client_secret': '426f5e748e6f00f8322fb4ccc9d3ea3857b48f6d',
+    'refresh_token': 'b0ed53054ca4b77a79caf083afc4a757bc89701a',
+    'grant_type': "refresh_token",
+    'f': 'json'
+}
+
+print("Requesting Token...\n")
+res = requests.post(auth_url, data=payload, verify=False)
+access_token = res.json()['access_token']
+print("Access Token = {}\n".format(access_token))
+
+header = {'Authorization': 'Bearer ' + access_token}
+param = {'per_page': 200, 'page': 1}
+my_dataset = requests.get(activites_url, headers=header, params=param).json()
+
+print(my_dataset[0]["name"])
+print(my_dataset[0]["map"]["summary_polyline"])
